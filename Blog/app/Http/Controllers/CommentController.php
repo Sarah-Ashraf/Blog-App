0a113688b21522a4c4431b6ce79ba7017d2ab258
Sarah_Ashraf <72573298+Sarah-Ashraf@users.noreply.github.com>
2021-08-11 05:50:32 +0200

    posts
    finished

---
+++ Blog/app/Http/Controllers/CommentController.php
@@ -0,0 +1,25 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use App\Models\Comment;
+use Illuminate\Support\Facades\Auth;
+
+class CommentController extends Controller
+{
+    public function addComment(Request $request)
+    {
+        $request ->validate([
+            'comment'=>'required'
+        ]);
+        //$post=Post::where('id',$id);
+        $comment=new Comment();
+        $comment->user_id=$request->user_id;;//$request->user()->id;
+        $comment->post_id=$request->post_id;
+        $comment->comment=$request->comment;
+        $comment->save();
+        return $comment . response()->json(['success','The Comment has been added'],200);
+
+    }
+}
